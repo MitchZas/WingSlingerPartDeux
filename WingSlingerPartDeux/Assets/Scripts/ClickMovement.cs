158ca02ba2a948c52e9ed37a388c853d2c29c764
MitchZas <mitchellzasa@gmail.com>
2026-09-07 16:32:12 -0400

    Updated the order position for the player

--- WingSlingerPartDeux/Assets/Scripts/ClickMovement.cs
+++ WingSlingerPartDeux/Assets/Scripts/ClickMovement.cs
@@ -18,7 +18,7 @@ public class ClickMovement : MonoBehaviour
     {
         if (!clickedObject.CompareTag("Customer")) return;
 
-        Player.transform.position = new Vector3(-0.48f, .43f, 0);
+        Player.transform.position = new Vector3(-0.61f, .58f, 0);
         customerClicked = true;
         Debug.Log(customerClicked);
     }
